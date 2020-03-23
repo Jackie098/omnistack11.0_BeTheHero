aa16e4dd688aabf51642a4c5582b87bbf4bb5d5d
Jackie098 <carlosmiranda19122@gmail.com>
2020-03-23 20:55:58 -0300

    add folder of frontend and basic react app

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.json({
+  event: 'Semana OmniStack 11.0',
+  student: 'Carlos Augusto',
+}));
+
+app.listen(3333);
