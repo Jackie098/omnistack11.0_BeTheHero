6a603fd1947bfc666f0c5f0404622d11fdbf6c35
Jackie098 <carlosmiranda19122@gmail.com>
2020-03-24 18:55:19 -0300

    add 'nodemon' and middleware for the code view 'JSON'

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => res.json({
   event: 'Semana OmniStack 11.0',
   student: 'Carlos Augusto',
