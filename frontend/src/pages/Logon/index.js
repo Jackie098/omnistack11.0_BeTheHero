41e726ba296fd26c3ac35f7e8db8a89eec176f79
Jackie098 <carlosmiranda19122@gmail.com>
2020-03-26 01:22:20 -0300

    created the profile page

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
@@ -18,10 +19,13 @@ export default function Logon() {
           <input placeholder="Sua ID" />
           <button type="submit" className="button">Login</button>
           
-          <a href="/register">
+          {/* É colocado o componente 'Link' do reac porque a tag 'a' recarrega
+            completamente a página, se fosse o caso, não teria o comportamento
+            de SPA como desejado. O component 'link' permite isso*/}
+          <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#E02041"/>
             Não tenho cadastro
-          </a>
+          </ Link>
         </form>
       </section>
 
