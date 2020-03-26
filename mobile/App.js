25d9a07600a6cab3dff014fe20f9f216a42b951c
Jackie098 <carlosmiranda19122@gmail.com>
2020-03-26 20:55:31 -0300

    initialized the pages to app mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello Omnistack 11.0</Text>
-      <Text style={styles.title}>Hello Omnistack 11.0</Text>
-    </View>
+    <Routes />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159CE',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 
-  title: {
-    color: '#FFF',
-    fontSize: 20,
-    fontWeight: 'bold'
-  },
-});
