cd37a58fd52b4d6313609f35c63d6de1c284eb92
Jackie098 <carlosmiranda19122@gmail.com>
2020-03-25 22:38:17 -0300

    Added module to and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,11 @@ import './global.css';
  * Não há necessidade de indicar o 'index.js', pois por não indicarmos
  * nada, ele sempre procura pelo arquivo padrão (index.js)
  */
-import Logon from './pages/Logon';
+import Routes from './routes';
 
 function App() {  
   return (
-    <Logon/>
+    <Routes />
   );
 }
 
