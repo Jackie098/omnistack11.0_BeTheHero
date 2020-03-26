64a47e30a7a20cdafee7fd85b94d56e0ca761f80
Jackie098 <carlosmiranda19122@gmail.com>
2020-03-26 03:46:25 -0300

    logging out of the application

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -11,8 +11,11 @@ import logoImg from '../../assets/logo.svg';
 export default function Profile() {
   const [incidents, setIncidents] = useState([]);
 
+  const history = useHistory();
+
   const ongId = localStorage.getItem('ongId');
   const ongName = localStorage.getItem('ongName');
+
 
   useEffect(() => {
     api.get('profile', {
@@ -33,9 +36,16 @@ export default function Profile() {
       });
 
       setIncidents(incidents.filter(incident => incident.id !== id));
+
     } catch (err) {
       alert('Erro ao deletar caso, tente novamente.');
     }
+  }
+
+  function handleLogout() {
+    localStorage.clear();
+
+    history.push('/');
   }
 
   return (
@@ -45,7 +55,7 @@ export default function Profile() {
         <span>Bem vinda, {ongName}</span>
 
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-        <button type="button">
+        <button onClick={handleLogout} type="button">
           <FiPower size={18} color="#E02041" />
         </button>
       </header>
