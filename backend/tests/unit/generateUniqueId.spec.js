fde4281b2e2a9d6313900cb74d60a0c758225cfd
Jackie098 <carlosmiranda19122@gmail.com>
2020-03-27 21:26:28 -0300

    Added 'jest' and created test folder

---
+++ backend/tests/unit/generateUniqueId.spec.js
@@ -0,0 +1,5 @@
+describe('Generate Unique ID', () => {  //Nome do teste
+  it('should generate an unique ID', () => {  //sobre o teste
+    expect(2 + 2).toBe(4);  //O que é como será testado
+  })
+})
