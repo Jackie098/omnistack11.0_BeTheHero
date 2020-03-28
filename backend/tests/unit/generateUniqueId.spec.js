6d1a0f8b4403d01606866801191fa140e97ea279
Jackie098 <carlosmiranda19122@gmail.com>
2020-03-28 17:01:53 -0300

    update test to verify the generated Id

--- backend/tests/unit/generateUniqueId.spec.js
+++ backend/tests/unit/generateUniqueId.spec.js
@@ -1,5 +1,9 @@
+const generateUniqueId = require('../../src/utils/generateUniqueId');
+
 describe('Generate Unique ID', () => {  //Nome do teste
   it('should generate an unique ID', () => {  //sobre o teste
-    expect(2 + 2).toBe(4);  //O que é como será testado
+    const id = generateUniqueId();
+    
+    expect(id).toHaveLength(8);  //O que é como será testado
   })
 })
